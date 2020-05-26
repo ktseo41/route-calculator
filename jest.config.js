c8326f5a782ad8747bc0fc7be10dbff32e68b36d
ktseo41 <ktseo41@gmail.com>
2020-05-27 00:41:25 +0900

    actions test

--- jest.config.js
+++ jest.config.js
@@ -68,15 +68,15 @@ module.exports = {
   //   "node_modules"
   // ],
 
-  // modulePaths: ["<rootDir>"],
+  modulePaths: ["<rootDir>/test"],
 
   // An array of file extensions your modules use
   moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  moduleNameMapper: {
-    "..": "<rootDir>",
-  },
+  // moduleNameMapper: {
+  //   "..": "<rootDir>",
+  // },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
